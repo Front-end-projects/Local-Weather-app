29d418792bfe6b969ea66ce792c8f4557458aa00
shirisha08 <shirisha.kasarla96@gmail.com>
2017-11-05 20:06:41 +0530

    added files

---
+++ javascript.js
@@ -0,0 +1,24 @@
+$(function(){
+	var c=false;
+	var apiData;
+
+
+	backgroundImg=[
+	"https://s.hswstatic.com/gif/thunderstorm-orig.jpg",
+	"http://www.radionacional.com.pe/sites/default/files/noticias/neblina_lima_senamhi.jpg"
+	"http://howlermag.com/wp-content/uploads/2016/12/rain.jpg",
+	"https://i.imgur.com/HRmlwoH.jpg",
+	"https://images.techhive.com/images/article/2016/11/fog_visibility_island-100692732-large.jpg",
+	"https://s3.envato.com/files/23923178/TropicalBlueSkyWithClouds590300.jpg",
+	"https://img00.deviantart.net/2729/i/2008/096/a/f/cloudy_sky_2_by_kuschelirmel_stock.jpg"
+]
+
+function displayTemp(F,c){
+	if(c)return Math.round((F-32)*(5/9)) + "&deg; c";
+	return Math.round(F)+"&deg; F";
+}
+
+//function render
+
+
+});
